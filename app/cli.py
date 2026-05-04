3316c914ea2f7ad1d2aaeb8a8333e00056ce6ac3
Farley Piedrahita Orozco <frlpiedrahita@gmail.com>
2026-05-04 16:47:46 -0500

    Agregar función para analizar argumentos de línea de comandos en cli.py

---
+++ app/cli.py
@@ -0,0 +1,32 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='VulnLab Scanner - Escáner educativo de vulnerabilidades OWASP Top 10.'
+    )
+
+    parser.add_argument(
+        "-u", "--url",
+        required=True,
+        help="URL del sitio web vulnerable a escanear (ejemplo: http://localhost:8080)"
+    )
+
+    parser.add_argument(
+        "-xss",
+        action="store_true",
+        help="Escanear vulberabilidades de Cross-Site Scripting (XSS)"
+    )
+
+    parser.add_argument(
+        "-sqli",
+        action="store_true",
+        help="Escanear vulnerabilidades de Inyección SQL (SQLi)"
+    )
+
+    parser.add_argument(
+        "--header",
+        action="store_true",
+        help="Revisar headers de seguridad"s"
+    )
+
+    return parser.parse_args()
