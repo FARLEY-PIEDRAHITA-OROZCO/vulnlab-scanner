1c986af1fead3b7f157ca1af5b7d3b2fbd24d403
Farley Piedrahita Orozco <frlpiedrahita@gmail.com>
2026-05-04 16:55:27 -0500

    Corregir argumentos de línea de comandos en cli.py para estandarizar nombres y mejorar la legibilidad

--- app/cli.py
+++ app/cli.py
@@ -12,21 +12,21 @@ def parse_args():
     )
 
     parser.add_argument(
-        "-xss",
+        "--xss",
         action="store_true",
         help="Escanear vulberabilidades de Cross-Site Scripting (XSS)"
     )
 
     parser.add_argument(
-        "-sqli",
+        "--sqli",
         action="store_true",
         help="Escanear vulnerabilidades de Inyección SQL (SQLi)"
     )
 
     parser.add_argument(
-        "--header",
+        "--headers",
         action="store_true",
-        help="Revisar headers de seguridad"s"
+        help="Revisar headers de seguridad"
     )
 
     return parser.parse_args()
