2f56ffb7313460d281503f38a10953c6754f91d8
Farley Piedrahita Orozco <frlpiedrahita@gmail.com>
2026-05-04 17:07:32 -0500

    Refactor manejo de errores y mensajes en main.py utilizando funciones de logger

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from app.cli import parse_args
 from app.utils.helpers import is_valid_url
+from app.utils.logger import info, success, error
 
 def main():
     args = parse_args()
 
     if not is_valid_url(args.url):
-        print("[ERROR] La URL debe comenzar con http:// o https://")
+        error("La URL debe comenzar con http:// o https://")
         return
 
     if args.all:
@@ -13,16 +14,16 @@ def main():
         args.sqli = True
         args.headers = True
 
-    print(f"[+] Objetivo: {args.url}")
+    info(f"[+] Objetivo: {args.url}")
 
     if args.xss:
-        print("[*] Escaneo XSS activado")
+        success("[*] Escaneo XSS activado")
 
     if args.sqli:
-        print("[*] Escaneo SQLi activado")
+        success("[*] Escaneo SQLi activado")
 
     if args.headers:
-        print("[*] Escaneo de headers activado")
+        success("[*] Escaneo de headers activado")
         
 if __name__ == "__main__":
     main()
---
+++ app/utils/logger.py
@@ -0,0 +1,15 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def info(message):
+    print(Fore.CYAN + "[INFO] " + message)
+
+def success(message):
+    print(Fore.GREEN + "[OK] " + message)
+
+def warning(message):
+    print(Fore.YELLOW + "[WARN] " + message)
+
+def error(message):
+    print(Fore.RED + "[ERROR] " + message)
